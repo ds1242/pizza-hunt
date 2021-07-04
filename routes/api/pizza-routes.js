0d21bb5d865a2c8c3bfe3cc1ea42a4e1b40a677a
dshaw1242 <david.shaw1242@gmail.com>
2021-07-04 11:13:21 -0600

    created pizza api routes

---
+++ routes/api/pizza-routes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+
+const { 
+    getAllPizza,
+    getPizzaById,
+    createPizza,
+    updatePizza,
+    deletePizza
+} = require('../../controllers/pizza-controller');
+
+
+// set up GET all and POST at /api/pizzas
+router
+    .router('/')
+    .get(getAllPizza)
+    .post(createPizza);
+
+
+
+// Set up GET one, PUT, and DELETE at /api/pizzas/:_id
+router 
+    .router('/:id')
+    .get(getPizzaById)
+    .put(updatePizza)
+    .delete(deletePizza);
+
+
+module.exports = router;
